eb15bc5e5f40d8f5ecc1100a58bdc2c1b4f2c35b
Samyak Sarnayak <samyak201@gmail.com>
2022-10-28 20:44:54 +0530

    implement basic working of contract

--- contracts/contracts/ShiroStore.sol
+++ contracts/contracts/ShiroStore.sol
@@ -2,7 +2,124 @@
 pragma solidity ^0.8.9;
 
 contract ShiroStore {
-    event NewFile(address owner, string cid, uint256 timestamp, uint256 validity);
+    event NewFile(
+        address owner,
+        string cid,
+        uint256 timestamp,
+        uint256 validity
+    );
     event DeleteFile(address owner, string cid, uint256 timestamp);
-    event ExtendLife(address owner, string cid, uint256 timestamp, uint256 extendBy);
+    event ExtendLife(
+        address owner,
+        string cid,
+        uint256 timestamp,
+        uint256 extendBy
+    );
+
+    struct File {
+        bool valid;
+        bool deleted;
+        string cid;
+        uint256 timestamp;
+        uint256 validity;
+    }
+
+    address[] owners;
+    mapping(address => File[]) store;
+
+    function memcmp(bytes memory a, bytes memory b)
+        internal
+        pure
+        returns (bool)
+    {
+        return (a.length == b.length) && (keccak256(a) == keccak256(b));
+    }
+
+    function strcmp(string memory a, string memory b)
+        internal
+        pure
+        returns (bool)
+    {
+        return memcmp(bytes(a), bytes(b));
+    }
+
+    function findFile(address owner, string memory cid)
+        internal
+        view
+        returns (File storage)
+    {
+        for (uint256 i = 0; i < store[owner].length; ++i) {
+            File storage file = store[owner][i];
+            if (file.valid && !file.deleted && strcmp(file.cid, cid)) {
+                return file;
+            }
+        }
+
+        revert("File not found or is deleted.");
+    }
+
+    function findOrCreateFile(address owner, string memory cid)
+        internal
+        returns (File storage)
+    {
+        for (uint256 i = 0; i < store[owner].length; ++i) {
+            File storage file = store[owner][i];
+            if (file.valid && !file.deleted && strcmp(file.cid, cid)) {
+                return file;
+            }
+        }
+
+        if (store[owner].length == 0) {
+            owners.push(owner);
+        }
+
+        return store[owner].push();
+    }
+
+    function deleteGivenFile(address owner, File storage file) internal {
+        file.deleted = true;
+
+        emit DeleteFile(owner, file.cid, block.timestamp);
+    }
+
+    function putFile(
+        address owner,
+        string memory cid,
+        uint256 validity
+    ) public payable {
+        require(bytes(cid).length == 46, "Invalid IPFS CID length.");
+
+        require(validity >= 3600, "Validity must be at least an hour.");
+
+        File storage file = findOrCreateFile(owner, cid);
+        file.valid = true;
+        file.deleted = false;
+        file.cid = cid;
+        file.timestamp = block.timestamp;
+        file.validity = validity;
+
+        emit NewFile(owner, cid, block.timestamp, validity);
+    }
+
+    function deleteFile(address owner, string memory cid) public {
+        File storage file = findFile(owner, cid);
+
+        deleteGivenFile(owner, file);
+    }
+
+    function garbageCollect() public {
+        for (uint256 ownerIdx = 0; ownerIdx < owners.length; ++ownerIdx) {
+            address owner = owners[ownerIdx];
+            for (
+                uint256 fileIdx = 0;
+                fileIdx < store[owner].length;
+                ++fileIdx
+            ) {
+                File storage file = store[owner][fileIdx];
+                if (file.timestamp + file.validity >= block.timestamp) {
+                    deleteGivenFile(owner, file);
+                }
+            }
+        }
+    }
 }
