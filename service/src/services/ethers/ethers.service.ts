dc60223674cd7331c4c1ef26222a446ee32f0575
Samyak S Sarnayak <samyak201@gmail.com>
2022-11-02 00:21:16 +0530

    read NewFile event

--- service/src/services/ethers/ethers.service.ts
+++ service/src/services/ethers/ethers.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { ethers } from 'ethers';
+import { BigNumber, ethers } from 'ethers';
+import { readFileSync } from 'fs';
+
+interface NewFile {
+  address: string;
+  cid: string;
+  timestamp: BigNumber;
+  validity: BigNumber;
+}
 
 @Injectable()
 export class EthersService {
@@ -10,14 +18,23 @@ export class EthersService {
     this.provider = new ethers.providers.JsonRpcProvider(
       process.env.QUICKNODE_URL,
     );
-    const contractAddress = '';
-    this.contract = new ethers.Contract(contractAddress, 'abi', this.provider);
-    this.contract.on('Log', (sender, message, event) => {
-      let info = {
-        sender: sender,
-        message: message,
+
+    const contractAddress = process.env.CONTRACT_ADDRESS;
+    const abi = JSON.parse(readFileSync('abi.json', 'utf8')).abi;
+    this.contract = new ethers.Contract(contractAddress, abi, this.provider);
+
+    this.contract.on('NewFile', (address, cid, timestamp, validity) => {
+      const info: NewFile = {
+        address,
+        cid,
+        timestamp,
+        validity,
       };
-      console.log(info);
+      this.handleNewFile(info);
     });
+  }
+
+  async handleNewFile(newFile: NewFile) {
+    console.log('Got a new file', newFile);
   }
 }
