4becec4a8a119bfe8a8439f304a8f7be417d23e9
Samyak Sarnayak <samyak201@gmail.com>
2022-11-15 00:32:29 +0530

    maybe fixed deploy script

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,8 +1,11 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const chainlinkTokenAddr = process.env.CHAINLINK_TOKEN_ADDR!;
+  const chainlinkOracleAddr = process.env.CHAINLINK_ORACLE_ADDR!;
+  const chainlinkUsdEthPriceFeedAddr = process.env.CHAINLINK_USD_ETH_PRICE_FEED_ADDR!;
   const ShiroStore = await ethers.getContractFactory("ShiroStore");
-  const shiroStore = await ShiroStore.deploy();
+  const shiroStore = await ShiroStore.deploy(chainlinkTokenAddr, chainlinkOracleAddr, chainlinkUsdEthPriceFeedAddr);
   await shiroStore.deployed();
 
   console.log(`ShiroStore deployed to ${shiroStore.address}`);
