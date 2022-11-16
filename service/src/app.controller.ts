636df58bb9f90e8019b2e47cbeb56dbf683b4acb
Samyak Sarnayak <samyak201@gmail.com>
2022-11-16 19:06:06 +0530

    "fix" price endpoint

--- service/src/app.controller.ts
+++ service/src/app.controller.ts
@@ -5,6 +5,7 @@ import {
   HttpStatus,
   Query,
 } from '@nestjs/common';
+import { BigNumber } from 'ethers';
 import { AppService } from './app.service.js';
 import { IpfsService } from './services/ipfs/ipfs.service.js';
 
@@ -15,7 +16,7 @@ export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly ipfs: IpfsService,
-  ) {}
+  ) { }
 
   @Get()
   getHello(): string {
@@ -70,19 +71,43 @@ export class AppController {
 
   // Returns price of storing the given CID.
   @Get('estimatePrice')
-  async estimatePrice(@Query('cid') cid?: string) {
+  async estimatePrice(
+    @Query('cid') cid?: string,
+    @Query('validity') validity?: number,
+  ) {
     if (cid === null || cid === undefined) {
       throw new HttpException(
         '`cid` was not given in query',
         HttpStatus.BAD_REQUEST,
       );
     }
-    const size = await this.ipfs.getFileSize(cid);
-    const sizeGB = size / 10 ** 9;
-    const price = PRICE_PER_GB_USD * sizeGB;
+    if (validity === null || validity === undefined) {
+      throw new HttpException(
+        '`validity` was not given in query',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    // this is 10^11
+    const bytePerUsd = BigNumber.from(10).pow(10);
+
+    const size = BigNumber.from(await this.ipfs.getFileSize(cid));
+    const sizeGB = size.div(BigNumber.from(10).pow(9));
+    // TODO: get this from price feed
+    const usdPerEth = BigNumber.from(1216);
+    const priceFeedDecimals = 0;
+
+    const numerator = size
+      .mul(BigNumber.from(10).pow(priceFeedDecimals))
+      .mul(BigNumber.from(10).pow(18))
+      .mul(validity)
+    const denom = bytePerUsd.mul(usdPerEth).mul(1000).mul(3600);
+    const price = numerator.div(denom);
+    console.log(numerator.toString(), denom.toString(), price.toString());
+
     console.log(
       `estimate (${cid}) - size: ${size} bytes (${sizeGB} GB), price: ${price}`,
     );
-    return { price: price, msg: "hey don't use this!" };
+    return { price: price.toString() };
   }
 }
