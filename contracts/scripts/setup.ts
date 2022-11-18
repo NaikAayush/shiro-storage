c8dd8d4cd28895ec732dee2de3e657ba529489ec
Aayush Naik <57558584+NaikAayush@users.noreply.github.com>
2022-11-18 10:46:47 +0530

    (fix) shiro store contract address

--- contracts/scripts/setup.ts
+++ contracts/scripts/setup.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 
 async function main() {
     let contract = await ethers.getContractFactory("ShiroStore");
-    let deployed = contract.attach("0xa7d14Ef4Ca44a1218efbba9E103E5Ad049dE7198");
+    let deployed = contract.attach("0x039a4697C8B6CD4dE6D2C0aef690e8c9073b67a6");
     let res = await deployed.putFile(
         "QmRxyew9ujdcByPwNaB8EVK8S5TBvJiRGuKPNEAvVMuGAm",
         7200,
